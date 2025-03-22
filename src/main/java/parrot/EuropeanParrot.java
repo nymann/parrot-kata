785b9a7366298e5a3f93cf12706b464719293c61
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 13:41:33 +0100

    Simplify

--- src/main/java/parrot/EuropeanParrot.java
+++ src/main/java/parrot/EuropeanParrot.java
@@ -6,18 +6,10 @@ public class EuropeanParrot extends Parrot {
     }
 
     public String getCry() {
-        return switch (type) {
-            case EUROPEAN -> "Sqoork!";
-            case AFRICAN -> "Sqaark!";
-            case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
-        };
+        return "Sqoork!";
     }
 
     public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return getBaseSpeed();
     }
 }
