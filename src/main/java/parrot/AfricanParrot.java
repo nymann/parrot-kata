705d4afdeb0fcd431681756b3dc51f0cfa77f8b6
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 13:36:48 +0100

    Add African Parrot

---
+++ src/main/java/parrot/AfricanParrot.java
@@ -0,0 +1,7 @@
+package parrot;
+
+public class AfricanParrot extends Parrot {
+    public AfricanParrot(int numberOfCoconuts, double voltage, boolean isNailed) {
+        super(ParrotTypeEnum.AFRICAN, numberOfCoconuts, voltage, isNailed);
+    }
+}
