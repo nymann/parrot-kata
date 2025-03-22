07caf3d8a8e0e074a4c4a879a92a090d6de900d7
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 13:27:39 +0100

    Re-order methods to make duplication obvious

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -18,14 +18,6 @@ public class Parrot {
         return new Parrot(type, numberOfCoconuts, voltage, isNailed);
     }
 
-    public double getSpeed() {
-        return switch (type) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
-    }
-
     private double getBaseSpeed(double voltage) {
         return Math.min(24.0, voltage * getBaseSpeed());
     }
@@ -43,6 +35,14 @@ public class Parrot {
             case EUROPEAN -> "Sqoork!";
             case AFRICAN -> "Sqaark!";
             case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
+        };
+    }
+
+    public double getSpeed() {
+        return switch (type) {
+            case EUROPEAN -> getBaseSpeed();
+            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
+            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
         };
     }
 }
