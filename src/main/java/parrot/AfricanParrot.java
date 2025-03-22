a2beff3826855b9d59c422a911d76f1d085839ca
Kristian Jakobsen <kristian@nymann.dev>
2025-03-22 13:42:16 +0100

    Remove unreachable branches

--- src/main/java/parrot/AfricanParrot.java
+++ src/main/java/parrot/AfricanParrot.java
@@ -6,18 +6,10 @@ public class AfricanParrot extends Parrot {
     }
 
     public String getCry() {
-        return switch (ParrotTypeEnum.AFRICAN) {
-            case EUROPEAN -> "Sqoork!";
-            case AFRICAN -> "Sqaark!";
-            case NORWEGIAN_BLUE -> voltage > 0 ? "Bzzzzzz" : "...";
-        };
+        return "Sqaark!";
     }
 
     public double getSpeed() {
-        return switch (ParrotTypeEnum.AFRICAN) {
-            case EUROPEAN -> getBaseSpeed();
-            case AFRICAN -> Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
-            case NORWEGIAN_BLUE -> (isNailed) ? 0 : getBaseSpeed(voltage);
-        };
+        return Math.max(0, getBaseSpeed() - getLoadFactor() * numberOfCoconuts);
     }
 }
